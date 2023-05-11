15f5ae778577552b4924231f3ca0ff17cc8b240f
Fabian Henzler <fabian.henzler@me.com>
2023-05-11 19:26:57 +0200

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="bg-[hsl(212, 45%, 89%)]">
+      <div className="">
+        <h1>Improve your front-end skills by building projects</h1>
+      </div>
+      <p>
+        Scan the QR code to visit Frontend Mentor and take your coding skills to
+        the next level
+      </p>
+    </main>
+  );
 }
